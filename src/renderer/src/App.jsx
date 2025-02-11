a784a006c14cacd1f9a73c1b108d191a71e73842
malikbarbier7 <159799378+malikbarbier7@users.noreply.github.com>
2025-02-11 14:26:40 +0100

    grid interface done

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -6,18 +6,16 @@ function App() {
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
   return (
-    <>
-      <div className="relative w-[300px] h-[300px]">
-        <div className="grid grid-cols-2 gap-[10px] p-[10px] pb-[1px]">
-          {[...Array(4)].map((_, i) => (
-            <div key={i} className="w-[130px] h-[130px] bg-black" />
-          ))}
-        </div>
-        <div className="text-xs text-right text-black pr-2 pt-1">
-          made by @M with love
-        </div>
+    <div className="flex flex-col items-center justify-center m-1 ">
+      <div className="grid grid-cols-2 gap-1 mb-0">
+        {[...Array(4)].map((_, i) => (
+          <div key={i} className="w-[130px] h-[120px] bg-black" />
+        ))}
       </div>
-    </>
+      <div className="w-full text-xs text-right text-black mt-1 pr-3">
+        made by @M with love
+      </div>
+    </div>
   )
 }
 
